c3171b6ab471c5b8e852fc0a625c9fc0856add7d
chicknfeet <131321513+chicknfeet@users.noreply.github.com>
2023-05-25 11:40:49 +0800

    Pages update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <a href="">Landing Page</a>
-            <a href="">Home</a>
-            <a href="">Profile</a>
-            <a href="">Events</a>
-            <a href="">News </a>
-        </nav>
-    </header>
+@extends('layout.app')
+
+@section('content')
     <h1>Welcome to Home Page</h1>
-</body>
-</html>
+@endsection
+
+@section('title')
+    Home Page
+@endsection
