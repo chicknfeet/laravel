6057ee8057e6e241c277d1478c204e27aa9dafd6
chicknfeet <east.carlangelomaniangap@gmail.com>
2023-06-08 10:59:01 +0800

    form update

--- database/migrations/2023_06_08_020119_create_activities_table.php
+++ database/migrations/2023_06_08_020119_create_activities_table.php
@@ -8,21 +8,24 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('accounts', function (Blueprint $table) {
+        Schema::create('activities', function (Blueprint $table) {
             $table->id();
-            $table->string('account');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('accounts');
+        Schema::dropIfExists('activities');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\landingController;
-use App\Http\Controllers\homeController;
-use App\Http\Controllers\profileController;
-use App\Http\Controllers\eventsController;
-use App\Http\Controllers\newsController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\contactController;
+use App\Http\Controllers\eventsController;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\landingController;
+use App\Http\Controllers\newsController;
+use App\Http\Controllers\profileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,21 +15,19 @@ use App\Http\Controllers\contactController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-
 Route::get('/landing',[landingController::class,'index'])->name('landing');
-
 Route::get('/home',[homeController::class,'index'])->name('home');
-
 Route::get('/profile',[profileController::class,'index'])->name('profile');
-
 Route::get('/events',[eventsController::class,'index'])->name('events');
-
 Route::get('/news',[newsController::class,'index'])->name('news');
 
 Route::get('/contact',[contactController::class,'index'])->name('contact');
-Route::get('contact/submit',[contactController::class,'submit'])->name('contact.submit');
+Route::post('/contact/submit',[contactController::class,'submit'])->name('contact.submit');
+
+Route::get('/login',[AuthController::class,'index'])->name('login');
+Route::post('/login',[AuthController::class,'login'])->name('login.submit');
